d31e0b6fe0644e5e365b2874e0fe16ad36e9d021
SIholin <iholin.sanna@gmail.com>
2018-12-02 21:52:13 +0200

    parantelua

--- IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -1,10 +1,9 @@
-
 package ohtu.intjoukkosovellus;
 
 public class IntJoukko {
 
     public final static int ALOITUSKOKO = 5, // aloitustalukon koko
-                            OLETUSKASVATUS = 5;  // luotava uusi taulukko on 
+            OLETUSKASVATUS = 5;  // luotava uusi taulukko on 
     // näin paljon isompi kuin vanha
     private int kokoero;     // Uusi taulukko on tämän verran vanhaa suurempi.
     private int[] jononLuvut;      // Joukon luvut säilytetään taulukon alkupäässä. 
@@ -30,8 +29,7 @@ public class IntJoukko {
         alkioidenLkm = 0;
         this.kokoero = OLETUSKASVATUS;
     }
-    
-    
+
     public IntJoukko(int kapasiteetti, int kasvatuskoko) {
         muuttujienTarkastaja(kapasiteetti, kasvatuskoko);
         jononLuvut = new int[kapasiteetti];
@@ -42,37 +40,41 @@ public class IntJoukko {
         this.kokoero = kasvatuskoko;
 
     }
+
     public void muuttujienTarkastaja(int kapasiteetti, int kasvatuskoko) {
-         if (kapasiteetti < 0) {
+        if (kapasiteetti < 0) {
             throw new IndexOutOfBoundsException("Kapasitteetti väärin");//heitin vaan jotain :D
         }
         if (kasvatuskoko < 0) {
             throw new IndexOutOfBoundsException("kapasiteetti2");//heitin vaan jotain :D
         }
     }
+
     public boolean lisaa(int luku) {
         if (alkioidenLkm == 0) {
             jononLuvut[0] = luku;
             alkioidenLkm++;
             return true;
-        } 
+        }
         if (!kuuluu(luku)) {
             return josLukuEiKuulu(luku);
         }
         return false;
     }
+
     public boolean josLukuEiKuulu(int luku) {
         jononLuvut[alkioidenLkm] = luku;
-            alkioidenLkm++;
-            if (alkioidenLkm % jononLuvut.length == 0) {
-                int[] taulukkoOld = new int[jononLuvut.length];
-                taulukkoOld = jononLuvut;
-                kopioiTaulukko(jononLuvut, taulukkoOld);
-                jononLuvut = new int[alkioidenLkm + kokoero];
-                kopioiTaulukko(taulukkoOld, jononLuvut);
-            }
-            return true;
+        alkioidenLkm++;
+        if (alkioidenLkm % jononLuvut.length == 0) {
+            int[] taulukkoOld = new int[jononLuvut.length];
+            taulukkoOld = jononLuvut;
+            kopioiTaulukko(jononLuvut, taulukkoOld);
+            jononLuvut = new int[alkioidenLkm + kokoero];
+            kopioiTaulukko(taulukkoOld, jononLuvut);
+        }
+        return true;
     }
+
     public boolean kuuluu(int luku) {
         int on = 0;
         for (int i = 0; i < alkioidenLkm; i++) {
@@ -97,16 +99,17 @@ public class IntJoukko {
 
     public boolean poistaminenOnnistuuko(int kohta) {
         if (kohta != -1) {
-        for (int j = kohta; j < alkioidenLkm - 1; j++) {
-               int apu = jononLuvut[j];
+            for (int j = kohta; j < alkioidenLkm - 1; j++) {
+                int apu = jononLuvut[j];
                 jononLuvut[j] = jononLuvut[j + 1];
                 jononLuvut[j + 1] = apu;
             }
             alkioidenLkm--;
-            return true;   
+            return true;
         }
         return false;
     }
+
     private void kopioiTaulukko(int[] vanha, int[] uusi) {
         for (int i = 0; i < vanha.length; i++) {
             uusi[i] = vanha[i];
@@ -118,7 +121,6 @@ public class IntJoukko {
         return alkioidenLkm;
     }
 
-
     @Override
     public String toString() {
         if (alkioidenLkm == 0) {
@@ -129,16 +131,18 @@ public class IntJoukko {
             return tulostuksenMuotoilu();
         }
     }
+
     public String tulostuksenMuotoilu() {
         String tuotos = "{";
-            for (int i = 0; i < alkioidenLkm - 1; i++) {
-                tuotos += jononLuvut[i];
-                tuotos += ", ";
-            }
-            tuotos += jononLuvut[alkioidenLkm - 1];
-            tuotos += "}";
-            return tuotos;
+        for (int i = 0; i < alkioidenLkm - 1; i++) {
+            tuotos += jononLuvut[i];
+            tuotos += ", ";
+        }
+        tuotos += jononLuvut[alkioidenLkm - 1];
+        tuotos += "}";
+        return tuotos;
     }
+
     public int[] toIntArray() {
         int[] taulu = new int[alkioidenLkm];
         for (int i = 0; i < taulu.length; i++) {
@@ -146,8 +150,9 @@ public class IntJoukko {
         }
         return taulu;
     }
-   public static IntJoukko rungonMuodostus(IntJoukko a, IntJoukko b, String tapahtuma) {
-       IntJoukko x = new IntJoukko();
+
+    public static IntJoukko rungonMuodostus(IntJoukko a, IntJoukko b, String tapahtuma) {
+        IntJoukko x = new IntJoukko();
         int[] aTaulu = a.toIntArray();
         int[] bTaulu = b.toIntArray();
         if (tapahtuma.equals("y")) {
@@ -157,24 +162,27 @@ public class IntJoukko {
         } else {
             return erotuksenToiminta(bTaulu, bTaulu, x);
         }
-        
-   } 
-   public static IntJoukko yhdisteenToiminta(int[] aJono, int[] bJono, IntJoukko c) {
-       for (int i = 0; i < aJono.length; i++) {
+
+    }
+
+    public static IntJoukko yhdisteenToiminta(int[] aJono, int[] bJono, IntJoukko c) {
+        for (int i = 0; i < aJono.length; i++) {
             c.lisaa(aJono[i]);
         }
         for (int i = 0; i < bJono.length; i++) {
             c.lisaa(bJono[i]);
         }
         return c;
-   }
+    }
+
     public static IntJoukko yhdiste(IntJoukko joukko1, IntJoukko joukko2) {
         String yhdiste = "y";
         return rungonMuodostus(joukko1, joukko2, yhdiste);
-        
+
     }
+
     public static IntJoukko leikkauksenToiminta(int[] taulu1, int[] taulu2, IntJoukko y) {
-         for (int i = 0; i < taulu1.length; i++) {
+        for (int i = 0; i < taulu1.length; i++) {
             for (int j = 0; j < taulu2.length; j++) {
                 if (taulu1[i] == taulu2[j]) {
                     y.lisaa(taulu2[j]);
@@ -188,8 +196,9 @@ public class IntJoukko {
         String leikkaus = "l";
         return rungonMuodostus(ensimmainen, toinen, leikkaus);
     }
+
     public static IntJoukko erotuksenToiminta(int[] tauluA, int[] tauluB, IntJoukko z) {
-         for (int i = 0; i < tauluA.length; i++) {
+        for (int i = 0; i < tauluA.length; i++) {
             z.lisaa(tauluA[i]);
         }
         for (int i = 0; i < tauluB.length; i++) {
@@ -197,10 +206,11 @@ public class IntJoukko {
         }
         return z;
     }
-    public static IntJoukko erotus (IntJoukko eka, IntJoukko toka) {
+
+    public static IntJoukko erotus(IntJoukko eka, IntJoukko toka) {
         String erotus = "e";
         return rungonMuodostus(eka, toka, erotus);
-       
+
     }
-        
+
 }
--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -19,48 +19,53 @@ public class TennisGame {
             m_score2 += 1;
         }
     }
+
     public String itIsDraw() {
-        if(null == playersPoints(m_score1)) {
+        if (null == playersPoints(m_score1)) {
             return "Deuce";
         } else {
             return playersPoints(m_score1) + "-All";
         }
     }
+
     public String whoIsTheWinner() {
-         int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) {
-                return "Advantage player1";
-            } else if (minusResult == -1) {
-                return "Advantage player2";
-            } else if (minusResult >= 2) {
-                return "Win for player1";
-            } else {
-                return "Win for player2";
-            }
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
     }
+
     public String playersPoints(int points) {
         switch (points) {
-                    case 0:
-                        return "Love";
-                    case 1:
-                        return "Fifteen";
-                    case 2:
-                        return "Thirty";
-                    case 3:
-                        return "Forty";
-                }
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
         return null;
     }
+
     public String stillGoing() {
         return playersPoints(m_score1) + "-" + playersPoints(m_score2);
     }
+
     public String getScore() {
         if (m_score1 == m_score2) {
-           return itIsDraw();
+            return itIsDraw();
         } else if (m_score1 >= 4 || m_score2 >= 4) {
-           return whoIsTheWinner();
+            return whoIsTheWinner();
         } else {
-           return stillGoing();
+            return stillGoing();
         }
     }
 }
